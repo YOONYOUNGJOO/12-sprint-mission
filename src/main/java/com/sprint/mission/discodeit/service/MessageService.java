852f9864c5db5e5568049dc5df4578fca7a481ac
YOON YOUNG JOO <ythalu5113@naver.com>
2026-03-28 17:52:37 +0900

    refactor: add repository interfaces and update service/channel design

--- src/main/java/com/sprint/mission/discodeit/service/MessageService.java
+++ src/main/java/com/sprint/mission/discodeit/service/MessageService.java
@@ -1,12 +1,14 @@
 package com.sprint.mission.discodeit.service;
 
+import com.sprint.mission.discodeit.entity.Channel;
 import com.sprint.mission.discodeit.entity.Message;
+import com.sprint.mission.discodeit.entity.User;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface MessageService {
-    Message save(Message message);
+    Message create(String content, UUID authorId, UUID channelId);
     Message findById(UUID id);
     List<Message> findAll();
     Message updateContent(UUID id, String content);
--- src/main/java/com/sprint/mission/discodeit/service/UserService.java
+++ src/main/java/com/sprint/mission/discodeit/service/UserService.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.UUID;
 
 public interface UserService {
-    User save(User user);
+    User create(String username, String email, String password);
     User findById(UUID id);
     List<User> findAll();
     User updateUsername(UUID id , String name);
