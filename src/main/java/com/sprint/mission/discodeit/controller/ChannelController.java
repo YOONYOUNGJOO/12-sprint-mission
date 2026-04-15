273ebb029032910ba539833f6b165617ad7c1766
YOON YOUNG JOO <ythalu5113@naver.com>
2026-04-15 19:14:02 +0900

    feat: implement channel and message APIs

--- src/main/java/com/sprint/mission/discodeit/controller/ChannelController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/ChannelController.java
@@ -1,4 +1,52 @@
 package com.sprint.mission.discodeit.controller;
 
+import com.sprint.mission.discodeit.dto.channel.ChannelResponse;
+import com.sprint.mission.discodeit.dto.channel.ChannelUpdateRequest;
+import com.sprint.mission.discodeit.dto.channel.CreatePrivateChannelRequest;
+import com.sprint.mission.discodeit.dto.channel.CreatePublicChannelRequest;
+import com.sprint.mission.discodeit.service.ChannelService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/channel")
 public class ChannelController {
+
+    private final ChannelService channelService;
+
+    @RequestMapping(value = "/public/create", method = RequestMethod.POST)
+    public ResponseEntity<ChannelResponse> publicCreate(@RequestBody CreatePublicChannelRequest publicChannelRequest){
+        ChannelResponse channelResponse = channelService.createPublicChannel(publicChannelRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(channelResponse);
+    }
+
+    @RequestMapping(value = "/private/create", method = RequestMethod.POST)
+    public ResponseEntity<ChannelResponse> privateCreate(@RequestBody CreatePrivateChannelRequest privateChannelRequest){
+        ChannelResponse channelResponse = channelService.createPrivateChannel(privateChannelRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(channelResponse);
+    }
+
+    @RequestMapping(value = "/public/update", method = RequestMethod.PATCH)
+    public ResponseEntity<ChannelResponse> publicUpdate(@RequestBody ChannelUpdateRequest channelUpdateRequest){
+        ChannelResponse channelResponse = channelService.update(channelUpdateRequest);
+        return ResponseEntity.ok(channelResponse);
+    }
+
+    @RequestMapping(value = "/delete/{id}" , method = RequestMethod.DELETE)
+    public ResponseEntity<Void> delete(@PathVariable UUID id){
+        channelService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value = "/findAll/{id}" , method = RequestMethod.GET)
+    public ResponseEntity<List<ChannelResponse>> findAllByUserId(@PathVariable UUID id){
+        List<ChannelResponse> channelResponseList = channelService.findAllByUserId(id);
+        return ResponseEntity.ok(channelResponseList);
+    }
 }
--- src/main/java/com/sprint/mission/discodeit/controller/MessageController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/MessageController.java
@@ -1,4 +1,47 @@
 package com.sprint.mission.discodeit.controller;
 
+
+import com.sprint.mission.discodeit.dto.message.MessageCreateRequest;
+import com.sprint.mission.discodeit.dto.message.MessageResponse;
+import com.sprint.mission.discodeit.dto.message.MessageUpdateRequest;
+import com.sprint.mission.discodeit.service.MessageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/message")
 public class MessageController {
+
+    private final MessageService messageService;
+
+    @RequestMapping(value = "/send", method = RequestMethod.POST)
+    public ResponseEntity<MessageResponse> send(@RequestBody MessageCreateRequest messageCreateRequest){
+        MessageResponse messageResponse = messageService.create(messageCreateRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(messageResponse);
+    }
+
+    @RequestMapping(value = "/update" , method = RequestMethod.PATCH)
+    public ResponseEntity<MessageResponse> update(@RequestBody MessageUpdateRequest messageUpdateRequest){
+        MessageResponse messageResponse = messageService.update(messageUpdateRequest);
+        return ResponseEntity.ok(messageResponse);
+    }
+
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Void> delete(@PathVariable UUID id){
+        messageService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value = "/findAll/{id}", method = RequestMethod.GET)
+    public ResponseEntity<List<MessageResponse>> findAllByChannelId(@PathVariable UUID id){
+        List<MessageResponse> messageResponseList = messageService.findAllByChannelId(id);
+        return ResponseEntity.ok(messageResponseList);
+    }
+
 }
